161189455d19d3215d1f60fd876f639ddc4697d7
Elmira PETIT <elledevelope@gmail.com>
2024-02-09 14:22:31 +0100

    Created contact form in `ContactController.php` and `contact/index.html.twig`

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Mime\Email;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
+        $formContact = $this->createForm(ContactType::class);
+        $formContact->handleRequest($request);
+
+        if ($formContact->isSubmitted() && $formContact->isValid()) {
+            $contactData = $formContact->getData();
+
+            // https://symfony.com/doc/current/mailer.html :
+            $email = (new Email()) //use Symfony\Component\Mime\Email;
+                ->from('conact@test.com')
+                ->to($contactData['email'])
+                ->subject($contactData['sujet'])
+                ->text($contactData['message'])
+                ->html('<p>' . $contactData['message'] . '</p>');
+
+            $mailer->send($email);
+
+            return $this->redirectToRoute('app_homepage');
+        }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'formContact' => $formContact,
         ]);
     }
 }
